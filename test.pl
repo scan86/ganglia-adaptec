c626fd4aca686a253bb2e960b646094f198fb64c
Dmitriy Anikin <d.anikin@creditnet.ru>
2014-09-22 16:54:03 +0400

    wip

--- test.pl
+++ test.pl
@@ -27,27 +27,49 @@ my $arcconf_bin = "/usr/StorMan/arcconf";
 my $arcconf_opts = "GETCONFIG 1";
 my $arcconf_cmdline = $arcconf_bin . " " . $arcconf_opts;
 
-
-open(my $fh, '<',  "arcconf");
+open(my $fh, "$arcconf_cmdline|") or die;
 
 my $temperature;
-my $onlineDrives = 0;
+my $drivesOnline = 0;
 
 
 while(<$fh>) {
  switch () {
-   case m/Temperature/ { $temperature = $1 if (/:\ (\d+)/) }
-   case m/Device is a Hard drive/ { checkDrive($_) }
+   case m/Temperature/ { $temperature = $1 if (/:\ (\d+)/) ;  next; }
+   case m/Device is a Hard drive/ {
+    if ((<$fh>) =~ /Online/) { $drivesOnline++ ; next; }
+   }
  }
-
 }
-
-sub checkDrive {
- if ((my $nextLine = <$fh>) =~ /Online/) { $onlineDrives++ }
-}
-
-print $temperature, "\n";
-print $onlineDrives, "\n";
-
-
 close($fh);
+
+
+&send_data("temperature", $temperature);
+&send_data("drives_online", $drivesOnline);
+
+sub send_data() {
+ my ($data_type, $value) = @_;
+
+ die if not $value;
+ die if not $data_type;
+ 
+ switch ($data_type) {
+  case "temperature" { send_temperature($value) }
+  case "drives_online" { send_drives_num($value) }
+ }
+}
+
+sub send_temperature {
+ my $val = shift;
+ my $cmd = "echo /usr/bin/gmetric -n \"adaptec.ctl0.temperature\" -v $val -t int8 -u \"Celcius\"";
+ system($cmd);
+ die if $? != 0;
+}
+sub send_drives_num {
+ my $val = shift;
+ my $cmd = "echo /usr/bin/gmetric -n \"adaptec.ctl0.drives_online\" -v $val -t int8 -u \"Number\"";
+ system($cmd);
+ die if $? != 0; 
+}
+
+
